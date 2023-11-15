84014ba93e15e388944355ca50d58ef2f00e8410
darkzense <torresjohn094@gmail.com>
2023-11-14 21:35:20 -0600

    updated editor style

--- js/editor.js
+++ js/editor.js
@@ -1,3 +1,4 @@
 var editor = ace.edit("editor");
-// editor.setTheme("ace/theme/monokai");
+editor.setTheme("ace/theme/crimson_editor");
 editor.session.setMode("ace/mode/assembly_x86");
+document.getElementById('editor').style.fontSize='24px';
