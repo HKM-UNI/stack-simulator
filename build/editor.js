e85bfa863e59422f8aac292c82b3696e67168efe
darkzense <torresjohn094@gmail.com>
2023-11-17 15:01:45 -0600

    use ace editor in typescript

--- build/editor.js
+++ build/editor.js
@@ -1,4 +1,5 @@
+import "ace";
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/crimson_editor");
 editor.session.setMode("ace/mode/assembly_x86");
-document.getElementById('editor').style.fontSize='24px';
+editor.setFontSize("24px");
---
+++ system/editor.ts
@@ -0,0 +1,6 @@
+import "ace";
+var editor = ace.edit("editor");
+
+editor.setTheme("ace/theme/crimson_editor");
+editor.session.setMode("ace/mode/assembly_x86");
+editor.setFontSize("24px");
